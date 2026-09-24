0b08edd20fdf1c2c504d77f334c645b9f89a1924
Anish-Rooj-cpu <anishrooj005@gmail.com>
2026-09-24 19:24:03 +0530

    Remove test scripts

--- test_highlighter.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-from PyQt5.QtWidgets import QApplication, QTextEdit
-from PyQt5.QtGui import QTextDocument
-from highlighter import VerilogHighlighter
-
-app = QApplication(sys.argv)
-doc = QTextDocument("module test;\nendmodule")
-highlighter = VerilogHighlighter(doc)
-print("Highlighter initialized")
