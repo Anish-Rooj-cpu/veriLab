23ef6c2944a174f52ac3ac86ff85b1a92bfee577
Anish-Rooj-cpu <anishrooj005@gmail.com>
2026-09-24 20:33:14 +0530

    Add support for multiple file selection (Shift/Ctrl+Click) and multi-delete in the Project Explorer

--- main.py
+++ main.py
@@ -287,6 +287,7 @@ class MainWindow(QMainWindow):
         self.tree.setColumnHidden(1, True)
         self.tree.setColumnHidden(2, True)
         self.tree.setColumnHidden(3, True)
+        self.tree.setSelectionMode(QTreeView.ExtendedSelection)
         self.tree.setStyleSheet("background-color: #252526; color: #CCCCCC; border: none; alternate-background-color: #2D2D30;")
         
         self.tree.setContextMenuPolicy(Qt.CustomContextMenu)
@@ -334,25 +335,36 @@ class MainWindow(QMainWindow):
         indexes = self.tree.selectedIndexes()
         if not indexes:
             return
-        index = indexes[0]
-        path = self.file_model.filePath(index)
-        is_dir = os.path.isdir(path)
+            
+        # Get unique selected paths
+        selected_paths = list(set([self.file_model.filePath(idx) for idx in indexes]))
         
         from PyQt5.QtWidgets import QMenu
         menu = QMenu()
         
-        new_file_act = menu.addAction("New File") if is_dir else None
-        rename_act = menu.addAction("Rename")
-        delete_act = menu.addAction("Delete")
-        
-        action = menu.exec_(self.tree.viewport().mapToGlobal(position))
-        
-        if action and action == new_file_act:
-            self.create_new_file_in_tree(path)
-        elif action == rename_act:
-            self.tree.edit(index)
-        elif action == delete_act:
-            self.delete_file_in_tree(path)
+        if len(selected_paths) == 1:
+            index = [i for i in indexes if i.column() == 0][0]
+            path = selected_paths[0]
+            is_dir = os.path.isdir(path)
+            
+            new_file_act = menu.addAction("New File") if is_dir else None
+            rename_act = menu.addAction("Rename")
+            delete_act = menu.addAction("Delete")
+            
+            action = menu.exec_(self.tree.viewport().mapToGlobal(position))
+            
+            if action and action == new_file_act:
+                self.create_new_file_in_tree(path)
+            elif action == rename_act:
+                self.tree.edit(index)
+            elif action == delete_act:
+                self.delete_files_in_tree(selected_paths)
+        else:
+            delete_act = menu.addAction(f"Delete {len(selected_paths)} items")
+            action = menu.exec_(self.tree.viewport().mapToGlobal(position))
+            
+            if action == delete_act:
+                self.delete_files_in_tree(selected_paths)
 
     def create_new_file_in_tree(self, dir_path):
         from PyQt5.QtWidgets import QInputDialog
@@ -364,19 +376,19 @@ class MainWindow(QMainWindow):
             except Exception as e:
                 QMessageBox.warning(self, "Error", str(e))
 
-    def delete_file_in_tree(self, path):
-        reply = QMessageBox.question(self, "Confirm Delete", 
-                                     f"Are you sure you want to delete {os.path.basename(path)}?",
-                                     QMessageBox.Yes | QMessageBox.No)
+    def delete_files_in_tree(self, paths):
+        msg = f"Are you sure you want to delete {os.path.basename(paths[0])}?" if len(paths) == 1 else f"Are you sure you want to delete {len(paths)} items?"
+        reply = QMessageBox.question(self, "Confirm Delete", msg, QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
-            try:
-                if os.path.isdir(path):
-                    import shutil
-                    shutil.rmtree(path)
-                else:
-                    os.remove(path)
-            except Exception as e:
-                QMessageBox.warning(self, "Error", str(e))
+            for path in paths:
+                try:
+                    if os.path.isdir(path):
+                        import shutil
+                        shutil.rmtree(path)
+                    else:
+                        os.remove(path)
+                except Exception as e:
+                    QMessageBox.warning(self, "Error", f"Could not delete {path}:\n{str(e)}")
 
     def tree_double_clicked(self, index):
         path = self.file_model.filePath(index)
